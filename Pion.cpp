4daf8d19534562bd31e2984fbde824f44b8cbd73
nejwaspar <58880911+nejwaspar@users.noreply.github.com>
2019-12-24 16:36:31 +0100

    L'ajout de la classe mére

---
+++ Pion.cpp
@@ -0,0 +1,62 @@
+#include "Pion.h"
+
+Pion::Pion()
+{}
+Pion::Pion(Case& position, bool normal):d_position{position},d_normal{normal}{}
+Pion::~Pion()
+{
+
+}
+
+virtual bool Pion::TypePion()
+{
+    return d_normal;
+}
+
+virtual Case& Pion::position const override
+{
+    return d_position;
+}
+
+void Pion:: sedeplace(deplacement& deplacement, char act){
+    switch(act)
+    {
+        case '8':
+            deplacement.Avancer(d_position);
+            d_position=deplacement.nouvelle_position();
+            break;
+        case '2':
+            deplacement.Reculer(d_position);
+            d_position=deplacement.nouvelle_position();
+            break;
+        case '6':
+            deplacement.TournerDroite(d_position);
+            d_position=deplacement.nouvelle_position();
+            break;
+        case '4':
+            deplacement.TournerGauche(d_position);
+            d_position=deplacement.nouvelle_position();
+            break;
+        if(d_type_moderne)
+       {
+            case '9':
+            deplacement.AvancerDroite(d_position);
+            d_position=deplacement.nouvelle_position();
+            break;
+            case '7':
+            deplacement.AvancerGauche(d_position);
+            d_position=deplacement.nouvelle_position();
+            break;
+            case '3':
+            deplacement.ReculerDroite(d_position);
+            d_position=deplacement.nouvelle_position();
+            break;
+            case '1':
+            deplacement.ReculerGauche(d_position);
+            d_position=deplacement.nouvelle_position();
+            break;
+       }
+
+}
+}
+
---
+++ Pion.h
@@ -0,0 +1,21 @@
+#ifndef PION_H
+#define PION_H
+
+
+class Pion
+{
+
+    public:
+        Pion();
+        Pion(Case& position, bool normal);
+        virtual ~Pion();
+        virtual bool TypePion();
+        virtual Case& position() const;
+        virtual void sedeplace(deplacement& deplacement,char act);
+
+    private:
+        bool d_normal;
+        Case& d_position;
+};
+
+#endif // PION_H
