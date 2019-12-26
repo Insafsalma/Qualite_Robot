4a6ea22dc715d72bdc7f03c01cc7ddb028bdd7c6
nejwaspar <58880911+nejwaspar@users.noreply.github.com>
2019-12-26 17:53:07 +0100

    Les méthodes du score et la méthode durée de vie

--- Score.cpp
+++ Score.cpp
@@ -1,16 +1,42 @@
+#include <iostream>
 #include "Score.h"
+#include <chrono>
+#include <ctime>
 
 Score::Score(int robot_detruit,int nbr_robots_terrain):d_nbr_robots_detruits{robot_detruit},d_nbr_robots_terrain{nbr_robots_terrain}
 { }
 
 Score::~Score()
 { }
+
 int Score::nbrRobotDetruits() const
 {
     return d_nbr_robots_detruits;
 }
 
+std::string Score::DureeDeVie() const
+{
+   time_t actuel = time(0);
+   tm *ltm = localtime(&actuel);
+   int end_min= 1 + ltm->tm_min ;
+   int end_sec= 1 + ltm->tm_sec;
+   int TotalSecond= ((end_min-d_start_min)*60)+(end_sec-d_start_sec);
+
+   return TotalSecond/60 +" : " +TotalSecond%60 ;
+
+}
 int Score::nbrRobotTerrain() const
 {
     return d_nbr_robots_terrain;
 }
+
+void Score:: ScoreFinal()const
+{
+
+    std::cout<<" Votre Score final : "<<std::endl;
+   std::cout<<" Votre durée de vie est : " << DureeDeVie()<<std::endl;
+    std::cout<<" Le nombre de robot que vous avez détruit : " << nbrRobotDetruits() <<std::endl;
+
+}
+
+
--- Score.h
+++ Score.h
@@ -1,5 +1,7 @@
 #ifndef SCORE_H
 #define SCORE_H
+#include <chrono>
+#include <ctime>
 
 
 class Score
@@ -9,9 +11,14 @@ class Score
         virtual ~Score();
         int nbrRobotDetruits() const;
         int nbrRobotTerrain() const;
+        std::string DureeDeVie() const;
+        void ScoreFinal()const;
     private:
     int d_nbr_robots_detruits;
     int d_nbr_robots_terrain;
+    int d_start_min;
+    int d_start_sec;
+    std::chrono::time_point<std::chrono::system_clock> d_start;
 };
 
 #endif // SCORE_H
