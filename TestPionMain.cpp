765e9b8328a25a6596debc27fd9742ed0b9089ce
batoul123 <58598912+batoul123@users.noreply.github.com>
2019-12-26 17:58:14 +0100

    Tests de Pion

---
+++ TestPionMain.cpp
@@ -0,0 +1,67 @@
+#include "Pion.h"
+#include "doctest.h"
+#include "deplacement.h"
+#include "Case.h"
+
+TEST_CASE(" vérifer le que le pion se dépéplace correctement ")
+{
+    std::ostream& ost{std::cout};
+    Case C{10.00,15.00};
+    deplacement  D {C};
+    Pion P{C,false};
+SUBCASE("pion avance correcetemnt")
+ {
+    Case C1{10.00,16.00};
+    P.sedeplace(D,8,ost);
+ REQUIRE_EQ(P.position(),C1);
+ }
+ SUBCASE("pion recule  correcetemnt")
+ {
+      Case C1{10.00,14.00}
+      P.sedeplacer(D,2,ost);
+      REQUIRE_EQ(P.position(),C1)
+ }
+  SUBCASE("pion tourne bien adroite")
+ {
+      Case C1{11.00,15.00}
+      P.sedeplacer(D,6,ost);
+      REQUIRE_EQ(P.position(),C1)
+ }
+ SUBCASE("pion tourne bien agauche")
+ {
+      Case C1{09.00,15.00}
+      P.sedeplacer(D,4,ost);
+      REQUIRE_EQ(P.position(),C1)
+ }
+ SUBCASE("pion type Normal")
+ {
+
+
+ SUBCASE("pion avanceDroite  ")
+ {
+   Case C1{11.00,16.00}
+      P.sedeplacer(D,9,ost);
+      REQUIRE_EQ(P.position(),C1)
+ }
+
+ SUBCASE("pion avanceGauche  ")
+ {
+   Case C1{09.00,15.00}
+      P.sedeplacer(D,7,ost);
+      REQUIRE_EQ(P.position(),C1)
+ }
+ SUBCASE("pion ReculeDroite  ")
+ {
+   Case C1{10.00,14.00}
+      P.sedeplacer(D,3,ost);
+      REQUIRE_EQ(P.position(),C1)
+ }
+ SUBCASE("pion ReculeGauche ")
+ {
+   Case C1{09.00,14.00}
+      P.sedeplacer(D,1,ost);
+      REQUIRE_EQ(P.position(),C1)
+ }
+ }
+}
+
