8bf3a7a56ce19fdba294fd2bbf8d78b3031d84bc
nejwaspar <58880911+nejwaspar@users.noreply.github.com>
2019-12-29 17:03:39 +0100

    Les tests sur le terrain

---
+++ TestMainTerrain.cpp
@@ -0,0 +1,25 @@
+#include "Terrain.h"
+#include "doctest.h"
+
+#include "Case.h"
+
+TEST_CASE("Verifier le terrain")
+{
+    Terrain t{3,4};
+    SUBCASE("taille  du terrain correcte")
+    {
+        REQUIRE_EQ(t.taille_terrain(),3*4);
+    }
+    SUBCASE("modification du terrain correcte")
+    {
+        t.modifier(5,5);
+        REQUIRE_EQ(t.taille_terrain(),25);
+    }
+    SUBCASE("Le terrain est rempli correctement")
+    {
+        Case c{1,1};
+        t.remplir_terrain();
+        REQUIRE_EQ(t.Cases().size()+1,25);
+        REQUIRE_EQ(t.Cases()[0],c);
+    }
+}
