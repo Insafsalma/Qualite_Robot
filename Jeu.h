2d87ead9c996c3b976e9ea39a237870b7ac98f57
Insafsalma <58422379+Insafsalma@users.noreply.github.com>
2019-12-31 16:10:12 +0100

    new update

--- Jeu.h
+++ Jeu.h
@@ -3,6 +3,7 @@
 #include "Terrain.h"
 #include "Pion.h"
 #include "Score.h"
+#include "debris.h"
 #include <vector>
 
 class Jeu
@@ -10,10 +11,10 @@ class Jeu
     public:
         Jeu(Pion& joueur,Terrain& terrain,Score& score);
         virtual ~Jeu();
-        void configurer_terrain();
         Terrain terrain_du_jeu() const;
-        void creer_terrain();
         Pion joueur() const;
+        void configurer_terrain();
+        void creer_terrain();
         void start_game(deplacement deplacement);
         void creer_debris();
         void end_Game();
@@ -22,6 +23,10 @@ class Jeu
         Pion d_joueur;
         std::vector<Pion> d_Robots;
         Score d_score_du_jeu;
+        std::vector<debris> d_Debris;
 };
+
+#endif // JEU_H
+
 
 #endif // JEU_H
