8d1e127d8234dd61ffd88ec030cd88688cdc2949
Insafsalma <58422379+Insafsalma@users.noreply.github.com>
2019-12-31 14:54:46 +0100

    nouvelle version

--- Jeu.cpp
+++ Jeu.cpp
@@ -1,11 +1,13 @@
 #include "Jeu.h"
 #include "Terrain.h"
 #include "Pion.h"
+#include "debris.h"
+#include "Score.h"
 #include <iostream>
 #include <vector>
-Jeu::Jeu(Pion& joueur,Terrain& terrain):d_joueur{joueur},d_terrain{terrain}
+
+Jeu::Jeu(Pion& joueur,Terrain& terrain,Score& score):d_joueur{joueur},d_terrain{terrain},d_score_du_jeu{score}
 {
-    //how do I do this??????
     int taille = d_terrain.taille_terrain();
     int colonnes=d_terrain.nombre_colonne();
     for(int i=0;i<colonnes/2;++i)
@@ -45,9 +47,30 @@ Pion Jeu::joueur() const
 {
     return d_joueur;
 }
+void Jeu::creer_debris()
+{
+    Case position {d_Robots[0].position()};
+    for(int i=1;i<=d_Robots.size();++i)
+    {
+
+        if(d_Robots[i].position()== position)
+        {
+            d_Robots.erase(d_Robots.begin()+i);
+        }
+    }
+    debris debris(position);
+}
+void Jeu::end_Game()
+{
+    if(d_Robots.size()== 0)
+    {
+
+    }
+}
 void Jeu::start_game(deplacement deplacement)
 {
     char action;
+    Score score_du_jeu{0,d_Robots.size()};
     std::cout<<"Le terrain contient : "<< d_terrain.taille_terrain()<< " cases"<<std::endl;
     std::cout<<"Le joueur est sur la case: "<<d_joueur.position().x()<<","<<d_joueur.position().y()<<std::endl;
     for(auto r : d_Robots)
@@ -59,8 +82,9 @@ void Jeu::start_game(deplacement deplacement)
     while(action!='q')
     {
         std::cin>>action;
+        score_du_jeu.DebutDeVie();
         d_joueur.sedeplace(deplacement,action);
-        std::cout<<"Le joueur s'est déplacé à la case: "<<d_joueur.position().x()<<","<<d_joueur.position().y()<<std::endl;
+        std::cout<<"Le joueur s'est dÃ©placÃ© Ã  la case: "<<d_joueur.position().x()<<","<<d_joueur.position().y()<<std::endl;
     }
-}
 
+}
