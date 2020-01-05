09cc3401ac919a7dfe9c880484af0cf6c16f03d1
nejwaspar <58880911+nejwaspar@users.noreply.github.com>
2020-01-05 15:23:55 +0100

    ajout des methode modifier et swap

--- Pion.cpp
+++ Pion.cpp
@@ -7,6 +7,21 @@ Pion::~Pion()
 {
 
 }
+
+void Pion::modifier(Pion pion)
+{
+    d_position=pion.position();
+    d_type_deplacement_normal=pion.Typedeplacement();
+    d_type_joueur=pion.TypePion();
+}
+
+void Pion::swap(Pion pion)
+{
+    Pion p1{d_position,d_type_deplacement_normal,d_type_joueur};
+    modifier(pion);
+    pion.modifier(p1);
+}
+
 bool Pion::Typedeplacement() const
 {
     return d_type_deplacement_normal;
